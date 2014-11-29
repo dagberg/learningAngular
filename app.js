7d48d16c6948574269826083fb727f6841ac4b2d
dagberg <ladagostino@gmail.com>
2014-11-28 18:30:35 -0600

    Finished Level 2, tabs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function(){
+(function(){
 
 	var app = angular.module('store', [ ]);
 
@@ -6,20 +6,38 @@ function(){
 		this.products = gems;
 	});
 
+	app.controller("PanelController", function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	});
+
 	var gems = [
 		{
 			name: 'Dodecahedron',
-			price: 2.95,
-			description: '. . . ',
+			price: 2,
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			image: 'Knucklehead crackers.jpg',
+			description: 'Dodecahedron is a twelve-sided motherfucker.',
+			specifications: 'It takes twelve sides to be a Dodecahedron.',
+			review: 'Dodecahedrons are the tits.',
 		},
 		{
 			name: 'Pentagonal Gem',
 			price: 5.95,
-			description: '. . . ',
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			image: 'Knucklehead crackers.jpeg',
+			description: 'Pentagons are a five-sided bitches.',
+			specifications: 'It takes five sides to be a pentagon.',
+			review: 'Pentagons are all right.',
 		}
 
 	]
